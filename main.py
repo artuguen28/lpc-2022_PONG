7323243c055dada3b817e62b1864fb40b1bae83c
augustoCSR7 <acsds.snf21@uea.edu.br>
2022-02-09 12:19:15 -0400

    Players

--- main.py
+++ main.py
@@ -40,7 +40,21 @@ hud.color("white")
 hud.penup()
 hud.hideturtle()
 hud.goto(0, 260)
-hud.write("0 : 0", align="center", font=("Arial", 24, "normal"))
+hud.write("0 : 0", align="center", font=("Bodoni MT Black", 24, "normal"))
+
+# Writing "Player 1" on the screen
+player1 = turtle.Turtle()
+player1.hideturtle()
+player1.goto(-240,260)
+player1.pencolor("green")
+player1.write("Player 1", align="left", font=("Bodoni MT Black", 24, "normal"))
+
+# Writing "Player 2" on the screen
+player2 = turtle.Turtle()
+player2.hideturtle()
+player2.goto(240,260)
+player2.pencolor("green")
+player2.write(" Player 2", align="right", font=("Bodoni MT Black", 24, "normal"))
 
 start_screen = turtle.Turtle()
 start_screen.speed(0)
@@ -103,7 +117,7 @@ while True:
     screen.update()
     
     while not(start_p):
-        start_screen.write("Press 'space' to start", align="center", font=("Arial", 24, "normal"))
+        start_screen.write("Press 'space' to start", align="center", font=("Bodoni MT Black", 24, "normal"))
     ball.showturtle()
     start_screen.clear()
 
@@ -111,9 +125,9 @@ while True:
     ball.sety(ball.ycor() + (ball.dy/2))
 
     # Colision with the upper wall
-    if ball.ycor() > 290:
+    if ball.ycor() > 250:
         #os.system("afplay bounce.wav&")
-        ball.sety(290)
+        ball.sety(250)
         ball.dy *= -1
 
     # Colision with the lower wall
@@ -122,6 +136,14 @@ while True:
         ball.sety(-280)
         ball.dy *= -1
 
+    # Colision with the paddle 1
+    if ball.xcor() < -330 and ball.ycor() < paddle_1.ycor() + 50 and ball.ycor() > paddle_1.ycor() - 50:
+        ball.dx *= -1
+
+    # Colision with the paddle 2
+    if ball.xcor() > 330 and ball.ycor() < paddle_2.ycor() + 50 and ball.ycor() > paddle_2.ycor() - 50:
+        ball.dx *= -1
+
     # Colision with the left wall
     if ball.xcor() < -390:
         score_2 += 1
@@ -129,6 +151,14 @@ while True:
         hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
         ball.goto(0, 0)
         ball.dx *= -1
+        # Showing that player 2 won
+        if score_2 > 5:
+            p2win = turtle.Turtle()  
+            p2win.hideturtle()
+            p2win.goto(0,0)
+            p2win.pencolor("yellow")
+            p2win.write("Player 2 wins", align="center", font=("Bodoni MT Black", 35))
+            break
 
     # Colision with the right wall
     if ball.xcor() > 390:
@@ -137,13 +167,13 @@ while True:
         hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
         ball.goto(0, 0)
         ball.dx *= -1
+        # Showing that player 1 won
+        if score_1 > 5:
+            p1win = turtle.Turtle()
+            p1win.hideturtle()
+            p1win.goto(0,0)
+            p1win.pencolor("yellow")
+            p1win.write("Player 1 wins", align="center", font=("Bodoni MT Black", 35)) 
+            break
 
-    # Colision with the paddle 1
-    if ball.xcor() < -330 and ball.ycor() < paddle_1.ycor() + 50 and ball.ycor() > paddle_1.ycor() - 50:
-        ball.dx *= -1
-
-    # Colision with the paddle 2
-    if ball.xcor() > 330 and ball.ycor() < paddle_2.ycor() + 50 and ball.ycor() > paddle_2.ycor() - 50:
-        ball.dx *= -1
-        
 turtle.done()
